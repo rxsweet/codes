9fae46e76856d5aa78cf1c03289ad5d987ef6a1c
rxsweet <105535100+rxsweet@users.noreply.github.com>
2023-01-29 14:26:32 +0800

    Delete fetchProxies-main directory

--- fetchProxies-main/utils/TGlist2Node/speedtest/clash_set_config.py
+++
@@ -1,7 +0,0 @@
-#调用自己的模块，先获取执行的目录，再import文件
-import sys
-sys.path.append(".") #执行目录地址
-from utils.clashspeedtest import set_config
-
-if __name__ == '__main__':
-    set_config.set_config('./sub/sources/TGsubNodeAll.yaml', './sub/TG64', './sub/TG.yaml')
