1a1a5eb0f6ff4f09b6915320c464110efd650bd0
rxsweet <105535100+rxsweet@users.noreply.github.com>
2023-08-30 17:01:45 +0800

    Add files via upload

---
+++ collectProxy-main/utils/staticSub/litespeedtest.py
@@ -0,0 +1,12 @@
+#调用自己的模块，先获取执行的目录，再import文件
+import sys
+sys.path.append(".") #执行目录地址
+from utils.litespeedtest import set_config
+from utils import tools
+
+if __name__ == '__main__':
+    #安装工具
+    tools.mmdb_install()
+    tools.subconverter_install()
+    #设置路径
+    set_config.set_config('./sub/sources/staticAll.yaml', './sub/litesrx64.txt', './sub/litesrx.yaml')
